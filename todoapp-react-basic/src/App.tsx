9c55385ef9b5085d500d2d90f9b8997ff1973b7e
Truoc Pham <truoc.phamkhac@asnet.com.vn>
2022-04-28 14:32:49 +0700

    feat: handle add a new todo task

--- todoapp-react-basic/src/App.tsx
+++ todoapp-react-basic/src/App.tsx
@@ -1,38 +1,39 @@
 import { Component } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
-import TodoFooter from './components/TodoFooter';
+import { ITask } from './interfaces/ITask';
+
 import TodoHeader from './components/TodoHeader';
 import TodoList from './components/TodoList';
+import TodoFooter from './components/TodoFooter';
+import Footer from './components/Footer';
 
 import 'todomvc-common/base.css';
 import 'todomvc-app-css/index.css';
 import './assets/css/App.css';
-import Footer from './components/Footer';
 
-class App extends Component {
+type AppProps = {};
+
+type AppState = {
+  todoList: ITask[];
+};
+class App extends Component<AppProps, AppState> {
   state = {
-    todoList: [
-      {
-        id: uuidv4(),
-        title: 'Task 1',
-        isCompleted: true,
-      },
-      {
-        id: uuidv4(),
-        title: 'Task 2',
-        isCompleted: false,
-      },
-    ],
+    todoList: [],
+  };
+
+  handleAddTodoTask = (task: ITask): void => {
+    this.setState((prevState) => ({
+      todoList: [...prevState.todoList, task],
+    }));
   };
 
   render = () => {
     return (
       <>
         <section className="todoapp">
-          <TodoHeader />
+          <TodoHeader addTodoTask={this.handleAddTodoTask} />
           <TodoList todoList={this.state.todoList} />
-          <TodoFooter />
+          {this.state.todoList.length > 0 && <TodoFooter />}
         </section>
         <Footer />
       </>
--- todoapp-react-basic/src/components/TodoHeader.tsx
+++ todoapp-react-basic/src/components/TodoHeader.tsx
@@ -1,16 +1,60 @@
-const TodoHeader = () => {
-  return (
-    <>
-      <header className="header">
-        <h1>todos</h1>
-        <input
-          className="new-todo"
-          placeholder="What needs to be done?"
-          autoFocus
-        />
-      </header>
-    </>
-  );
+import { v4 as uuidv4 } from 'uuid';
+import { ChangeEvent, Component, KeyboardEvent } from 'react';
+
+import { ITask } from '../interfaces/ITask';
+
+type TodoHeaderProps = {
+  addTodoTask: (task: ITask) => void;
 };
+
+type TodoHeaderState = {
+  text: string;
+};
+
+class TodoHeader extends Component<TodoHeaderProps, TodoHeaderState> {
+  state = {
+    text: '',
+  };
+
+  handleAddTodoTask = (event: KeyboardEvent<HTMLInputElement>) => {
+    const title: string = this.state.text;
+
+    if (event.key === 'Enter' && title) {
+      this.props.addTodoTask({
+        id: uuidv4(),
+        title,
+        isCompleted: false,
+      });
+
+      this.setState({
+        text: '',
+      });
+    }
+  };
+
+  render = (): JSX.Element => {
+    return (
+      <>
+        <header className="header">
+          <h1>todos</h1>
+          <input
+            className="new-todo"
+            placeholder="What needs to be done?"
+            autoFocus
+            value={this.state.text}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              this.setState({
+                text: e.target.value,
+              });
+            }}
+            onKeyDown={(e: KeyboardEvent<HTMLInputElement>) =>
+              this.handleAddTodoTask(e)
+            }
+          />
+        </header>
+      </>
+    );
+  };
+}
 
 export default TodoHeader;
